b26c6365d5028567bac8e7f9d63fcaf624c8f994
RadiantEngr <onaolapo.ak@gmail.com>
2021-01-28 23:32:26 +0100

    add endpoint to create admin

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import signUp from "../controllers/userSignup";
 import verifyUser from "../controllers/verifyUser";
+import createAdmin from "../controllers/createAdmin";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 
 router.put("/verifyuser/:email", verifyUser);
+
+router.post("/admin", createAdmin);
 
 export default router;
