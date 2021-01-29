e23ba984e9f8486e449f53f1554e50ef86f86346
RadiantEngr <onaolapo.ak@gmail.com>
2021-01-29 01:55:22 +0100

    create endpoint for user to login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import signUp from "../controllers/userSignup";
 import verifyUser from "../controllers/verifyUser";
 import createAdmin from "../controllers/createAdmin";
+import userLogin from "../controllers/userLogin";
 
 const router = express.Router();
 
@@ -10,5 +11,7 @@ router.post("/signup", signUp);
 router.put("/verifyuser/:email", verifyUser);
 
 router.post("/admin", createAdmin);
+
+router.post("/userlogin", userLogin);
 
 export default router;
