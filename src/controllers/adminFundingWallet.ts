566e49ffda79b4ba85ea2aec07e651ae2c050a96
RadiantEngr <onaolapo.ak@gmail.com>
2021-01-31 09:15:40 +0100

    create endpoint to allow admin fund a user account

--- src/controllers/adminFundingWallet.ts
+++ src/controllers/adminFundingWallet.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { User } from "../models/user";
 import { Admin } from "../models/admin";
-import { Transaction } from "../models/transaction";
 
 const fundByAdmin = async (req: Request, res: Response) => {
   try {
